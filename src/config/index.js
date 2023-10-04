722e2f0e2bdfc9ea6deb09a159320a13b95b12b5
mohdashiqtp <86231775+mohdashiqtp@users.noreply.github.com>
2023-10-04 17:35:57 +0530

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,31 +1,15 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-// set production variable. This will be called when deployed to a live host
-const isProduction = process.env.NODE_ENV === 'production';
+// Set up the PostgreSQL connection string
+const connectionString = process.env.DATABASE_URL;
 
-// configuration details
-const connectionString = `postgresql://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
-
-
-const sequelizeOptions = {
+const sequelize = new Sequelize(connectionString, {
   dialect: 'postgres',
-  logging: isProduction ? false : true, // Disable logging in production
-};
-
-if (isProduction) {
-  // Use connection string in production
-  sequelizeOptions.dialectOptions = {
-    connectionString: connectionString,
-  };
-} else {
-  // Use individual properties for development
-  sequelizeOptions.host = process.env.DB_HOST;
-  sequelizeOptions.database = process.env.DB_DATABASE;
-  sequelizeOptions.username = process.env.USERNAME;
-  sequelizeOptions.password = process.env.PASSWORD;
-  sequelizeOptions.port = process.env.DB_PORT;
-}
+dialectOptions: {
+  ssl: true,
+},
+logging: false,
+});
 
 module.exports = sequelize;
-
