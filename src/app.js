49a8cbbb1324bc808257bbf6a5822e1c9d3f9223
mohdashiqtp <86231775+mohdashiqtp@users.noreply.github.com>
2023-10-04 19:13:34 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -40,4 +40,11 @@ app.get('/', async (req, res) => {
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
 
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  /* eslint-disable no-console */
+  console.log(`Listening: http://localhost:${port}`);
+  /* eslint-enable no-console */
+});
+
 module.exports = app;
