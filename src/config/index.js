9c24e59b609590650bd1b9f23bbccb66d9df1341
mohdashiqtp <86231775+mohdashiqtp@users.noreply.github.com>
2023-10-04 20:04:12 +0530

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // Set up the PostgreSQL connection string
-const connectionString = process.env.DATABASE_URL;
+const connectionString = "postgres://default:WJZzQyTr50tB@ep-rapid-forest-95611418.us-east-1.postgres.vercel-storage.com:5432/verceldb";
 
 const sequelize = new Sequelize(connectionString, {
   dialect: 'postgres',
